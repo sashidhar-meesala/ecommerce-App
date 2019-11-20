8f1bceea3b0cd1f3cd8c4392fc6949977779d532
Sashidhar Meesala <n01324566@nj-00118-007.humber.org>
2019-11-19 21:49:57 -0500

    second

--- js/index.js
+++ js/index.js
@@ -28,7 +28,9 @@ function descreaseQuantityP(){
 
 function extendedPriceP(){
      let price= parseInt(qtyLabelP.innerHTML) * 40.99;
-     pantPriceLabel.innerHTML=price;
+     priceINT=price.toFixed(2);
+     pantPriceLabel.innerHTML='$'+priceINT;
+     return price;
 }
 
 
@@ -50,12 +52,31 @@ function descreaseQuantityS(){
 
 function extendedPriceS(){
      let price= parseInt(qtyLabelS.innerHTML) * 10.99;
-     shirtPriceLabel.innerHTML=price;
+     priceINT=price.toFixed(2);
+     shirtPriceLabel.innerHTML='$'+priceINT;
+     return price;
+}
+
+function calculateAll(){
+    let subtotal = (extendedPriceP()+extendedPriceS()).toFixed(2);
+    document.getElementById("subtotal").innerHTML=subtotal;
+    let tax= (0.13*subtotal).toFixed(2);
+    document.getElementById("tax").innerHTML=tax;
+    let total = (subtotal+ tax);
+    document.getElementById("total").innerHTML=Total1;
+}
+
+
+function applycoupon(){
+    const coupon=window.prompt("please enter the coupon code");
+    
 }
 
 
 
-minusButtonP.addEventListener("click",descreaseQuantityP);
-plusButtonP.addEventListener("click",increaseQuantityP);
-minusButtonS.addEventListener("click",descreaseQuantityS);
-plusButtonS.addEventListener("click",increaseQuantityS);
+    minusButtonP.addEventListener("click",descreaseQuantityP);
+    plusButtonP.addEventListener("click",increaseQuantityP);
+    minusButtonS.addEventListener("click",descreaseQuantityS);
+    plusButtonS.addEventListener("click",increaseQuantityS);
+    document.getElementById("Calculate").addEventListener("click",calculateAll);
+    
